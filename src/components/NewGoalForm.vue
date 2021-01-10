7d15c395f82a0c7561b00752b2322d4a5405e3e5
SOrmiston <sean.david.ormiston@gmail.com>
2021-01-09 22:11:35 -0500

    form checked to cover all data props

--- src/components/NewGoalForm.vue
+++ src/components/NewGoalForm.vue
@@ -1,7 +1,7 @@
 <template>
   <form @submit.prevent="logForm">
     <div class="form-control">
-      <input id="title" type="text" placeholder="Goal" />
+      <input id="title" type="text" placeholder="Goal" v-model.trim="title" />
     </div>
     <div class="form-control">
       <h3>Track</h3>
@@ -9,18 +9,41 @@
         for="health"
         >Health</label
       >
-      <input type="radio" id="learning" value="Learning" v-model="track" /><label
-        for="learnign"
-        >Learning</label
+      <input
+        type="radio"
+        id="learning"
+        value="Learning"
+        v-model="track"
+      /><label for="learning">Learning</label>
+      <input
+        type="radio"
+        id="relationships"
+        value="Relationships"
+        v-model="track"
+      /><label for="relationships">Relationships</label>
+      <input
+        type="radio"
+        id="self-growth"
+        value="Self-Growth"
+        v-model="track"
+      /><label for="self-growth">Self-Growth</label>
+      <input type="radio" id="work" value="Work" v-model="track" /><label
+        for="work"
+        >Work</label
       >
     </div>
     <div class="form-control">
       <label for="desc">Description</label>
-      <textarea id="desc" cols="30" rows="5"></textarea>
+      <textarea
+        id="desc"
+        cols="30"
+        rows="5"
+        v-model.trim="description"
+      ></textarea>
     </div>
     <div class="form-control">
       <label for="start">Start</label>
-      <input type="date" id="start" :value="today" :min="today" />
+      <input type="date" id="start" :min="today" v-model="startTime" />
     </div>
     <div class="form-control">
       <h3>Time</h3>
@@ -33,11 +56,23 @@
         How much time will you spend on this goal each regular session?
         (duration)
       </h3>
-      <input type="number" id="hours" max="8" /><label for="hours">Hours</label>
-      <input type="number" id="minutes" max="60" step="15" /><label
-        for="minutes"
-        >Minutes</label
+      <input type="number" id="hours" max="8" /><label
+        for="hours"
+        :disabled="noDuration"
+        >Hours</label
       >
+      <input
+        type="number"
+        id="minutes"
+        max="60"
+        step="15"
+        :disabled="noDuration"
+      /><label for="minutes">Minutes</label>
+      <input type="checkbox" id="no-duration" v-model="noDuration" />
+      <label for="no-duration">No specific duration</label>
+      <span @click.stop="toggleTooltip">?</span>
+      <p v-if="tooltip">Helpful reminder.  <span @click="toggleTooltip">Dismiss.</span></p>
+      
     </div>
     <div class="form-control">
       <h3>Cycle</h3>
@@ -53,56 +88,111 @@
           <option>Third</option>
           <option>Fourth</option>
         </select>
-        <input
-          type="checkbox"
-          value="Monday"
-          id="Monday"
-          v-model="cycleDaysOfWeek"
-        />
-        <label for="Monday">Monday</label>
-        <input
-          type="checkbox"
-          value="Tuesday"
-          id="Tuesday"
-          v-model="cycleDaysOfWeek"
-        />
-        <label for="Tuesday">Tuesday</label>
-        <input
-          type="checkbox"
-          value="Wednesday"
-          id="Wednesday"
-          v-model="cycleDaysOfWeek"
-        />
-        <label for="Wednesday">Wednesday</label>
-        <input
-          type="checkbox"
-          value="Thursday"
-          id="Thursday"
-          v-model="cycleDaysOfWeek"
-        />
-        <label for="Thursday">Thursday</label>
-        <input
-          type="checkbox"
-          value="Friday"
-          id="Friday"
-          v-model="cycleDaysOfWeek"
-        />
-        <label for="Friday">Friday</label>
-        <input
-          type="checkbox"
-          value="Saturday"
-          id="Saturday"
-          v-model="cycleDaysOfWeek"
-        />
-        <label for="Saturday">Saturday</label>
-        <input
-          type="checkbox"
-          value="Sunday"
-          id="Sunday"
-          v-model="cycleDaysOfWeek"
-        />
-        <label for="Sunday">Sunday</label>
-        <span v-if="cycle === 'Monthly'">Of the Month</span>
+        <div v-if="cycle !== 'Monthly'" class="dow-checkboxes">
+          <input
+            type="checkbox"
+            value="Monday"
+            id="Monday"
+            v-model="cycleDaysOfWeek"
+          />
+          <label for="Monday">Monday</label>
+          <input
+            type="checkbox"
+            value="Tuesday"
+            id="Tuesday"
+            v-model="cycleDaysOfWeek"
+          />
+          <label for="Tuesday">Tuesday</label>
+          <input
+            type="checkbox"
+            value="Wednesday"
+            id="Wednesday"
+            v-model="cycleDaysOfWeek"
+          />
+          <label for="Wednesday">Wednesday</label>
+          <input
+            type="checkbox"
+            value="Thursday"
+            id="Thursday"
+            v-model="cycleDaysOfWeek"
+          />
+          <label for="Thursday">Thursday</label>
+          <input
+            type="checkbox"
+            value="Friday"
+            id="Friday"
+            v-model="cycleDaysOfWeek"
+          />
+          <label for="Friday">Friday</label>
+          <input
+            type="checkbox"
+            value="Saturday"
+            id="Saturday"
+            v-model="cycleDaysOfWeek"
+          />
+          <label for="Saturday">Saturday</label>
+          <input
+            type="checkbox"
+            value="Sunday"
+            id="Sunday"
+            v-model="cycleDaysOfWeek"
+          />
+          <label for="Sunday">Sunday</label>
+        
+        </div>
+        <div v-if="cycle === 'Monthly'" class="dow-radio">
+          <input
+            type="radio"
+            value="Monday"
+            id="Monday"
+            v-model="cycleOnceMonthlyDay"
+          />
+          <label for="Monday">Monday</label>
+          <input
+            type="radio"
+            value="Tuesday"
+            id="Tuesday"
+            v-model="cycleOnceMonthlyDay"
+          />
+          <label for="Tuesday">Tuesday</label>
+          <input
+            type="radio"
+            value="Wednesday"
+            id="Wednesday"
+            v-model="cycleOnceMonthlyDay"
+          />
+          <label for="Wednesday">Wednesday</label>
+          <input
+            type="radio"
+            value="Thursday"
+            id="Thursday"
+            v-model="cycleOnceMonthlyDay"
+          />
+          <label for="Thursday">Thursday</label>
+          <input
+            type="radio"
+            value="Friday"
+            id="Friday"
+            v-model="cycleOnceMonthlyDay"
+          />
+          <label for="Friday">Friday</label>
+          <input
+            type="radio"
+            value="Saturday"
+            id="Saturday"
+            v-model="cycleOnceMonthlyDay"
+          />
+          <label for="Saturday">Saturday</label>
+          <input
+            type="radio"
+            value="Sunday"
+            id="Sunday"
+            v-model="cycleOnceMonthlyDay"
+          />
+          <label for="Sunday">Sunday</label>
+        
+        </div>
+        <span v-if="cycle === 'Monthly'">-- of the Month</span>
       </div>
     </div>
     <div class="form-control">
@@ -117,11 +207,17 @@
 export default {
   data() {
     return {
+      title: "",
+      description: "",
+      startTime: this.$store.getters.currentTime.format("YYYY-MM-DD"),
       noTime: false,
+      noDuration: false,
       cycle: "Weekly",
       cycleDaysOfWeek: [],
+      cycleOnceMonthlyDay: '',
       cycleDayOfMonth: "First",
-      track: null
+      track: null,
+      tooltip: false
     };
   },
   computed: {
@@ -129,14 +225,18 @@ export default {
       return this.$store.getters.currentTime.format("YYYY-MM-DD");
     },
     oneMonthFromToday() {
-      return this.$store.getters.currentTime.add(1, "month");
-    }
+      return this.$store.getters.currentTime.add(1, "month").format("YYYY-MM-DD");
+    },
+    
   },
   methods: {
     logForm() {
       console.log(this.data);
+    },
+    toggleTooltip() {
+      this.tooltip = !this.tooltip
     }
-  }
+  },
 };
 </script>
 
