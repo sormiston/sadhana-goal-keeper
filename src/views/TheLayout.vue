b371466e366aeb47d3626d6bc6fd53d8ae3e6300
SOrmiston <sean.david.ormiston@gmail.com>
2021-01-11 00:05:34 -0500

    all set for styling static layout element

---
+++ src/views/TheLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <section>
+      <router-view />
+    </section>
+  </main>
+</template>
+
+<style scoped>
+main {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  background-color: var(--primary);
+}
+
+section {
+  background-color: var(--backdrop);
+  margin-top: 60px;
+  min-height: calc(100vh - 60px);
+  padding-top: 10px;
+  overflow: hidden;
+}
+
+@media screen and (min-width: 767px) {
+  section {
+    margin-left: 75px;
+    border-top-left-radius: 50px;
+    padding-top: 40px;
+    padding-left: 74px;
+    
+  }
+}
+</style>
