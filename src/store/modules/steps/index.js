2e4c971082f01ec6e2327b9f471d47b07f3995f2
SOrmiston <sean.david.ormiston@gmail.com>
2021-01-12 03:17:09 -0500

    repairs scope of getTodaysSteps get request

--- src/store/modules/steps/index.js
+++ src/store/modules/steps/index.js
@@ -22,8 +22,12 @@ export default {
     async getTodaysSteps(context) {
       const userId = context.rootGetters.userId;
       try {
+        const today = context.rootGetters.currentTime.format("YYYY-MM-DD");
+        const tomorrow = context.rootGetters.currentTime
+          .add(1, "d")
+          .format("YYYY-MM-DD");
         const response = await axios(
-          `${userId}/steps.json?orderBy=%22dateTime%22&endAt=%22${context.rootGetters.currentTime.format()}%22`
+          `${userId}/steps.json?orderBy=%22dateTime%22&startAt=%22${today}%22&endAt=%22${tomorrow}%22`
         );
         if (response.status < 200 || response.status >= 300)
           throw new Error(response.status);
@@ -49,12 +53,14 @@ export default {
             done: true
           }
         );
-        console.log('markDone steps state')
-          console.log(response);
+        console.log("markDone steps state");
+        console.log(response);
         if (response.status < 200 || response.status >= 300)
           throw new Error(response.status);
         else {
-          context.dispatch('goals/updateGoalStepCount', payload.goalId, { root: true })
+          context.dispatch("goals/updateGoalStepCount", payload.goalId, {
+            root: true
+          });
         }
       } catch (error) {
         console.error(error.message);
