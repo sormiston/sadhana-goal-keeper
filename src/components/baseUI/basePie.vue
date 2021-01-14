5ebbcd1b6f00c1c4c9efbf92384591f79470fc84
SOrmiston <sean.david.ormiston@gmail.com>
2021-01-13 20:11:03 -0500

    first week rendering OK in stepsDonePerDiePieCard

--- src/components/baseUI/basePie.vue
+++ src/components/baseUI/basePie.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="pie-chart-wrapper">
-    <Chart
-      type="pie"
-      :data="chartData"
-      :options="options"
-      :width="width"
-      :height="height"
-      ref="chart"
-    />
-  </div>
-
+  <img v-if="freeDay" src="../../assets/bike.png" />
+  <Chart
+    v-else
+    type="pie"
+    :data="pooledData"
+    :options="options"
+    :width="width"
+    :height="height"
+    style="width: '50px'"
+    ref="chart"
+  />
 </template>
 
 <script>
@@ -17,39 +17,61 @@ import Chart from "primevue/chart";
 export default {
   props: {
     chartData: {
-      type: Object,
+      type: Array,
       required: true
-    },
-  
+    }
   },
   data() {
     return {
       width: 50,
       height: 50,
       options: {
-          responsive: false,
-          animation: {
-            animateRotate: true
-          },
-          tooltips: {
-            enabled: false
-          }
+        responsive: false,
+        animation: {
+          animateRotate: true
+        },
+        tooltips: {
+          enabled: false
+        }
       }
     };
+  },
+  computed: {
+    pooledData() {
+      const data = this.chartData;
+      return {
+        datasets: [
+          {
+            data,
+            backgroundColor: ["#6E34EA", "#E3E3E3"],
+            borderWidth: 0
+          }
+        ]
+      };
+    },
+    freeDay() {
+      return this.chartData.every((val) => val === 0);
+    }
   },
 
   components: {
     Chart
   },
   mounted() {
-    console.log(this.$refs.chart);
+    console.log(this.chartData[0]);
   }
 };
 </script>
 
 <style scoped>
-.pie-chart-wrapper {
-  margin: 0 auto;
+img {
+  height: 50px;
+  width: 50px;
+}
+.p-chart {
+  height: 60px;
+  display: flex;
+  justify-content: center;
 }
 </style>
 
--- src/components/dashboard/StepsDonePerDayPieCard.vue
+++ src/components/dashboard/StepsDonePerDayPieCard.vue
@@ -1,44 +1,40 @@
 <template>
   <base-card title="Steps done per day">
-  <div class="calendar-grid">
-    <div class="blank"></div>
-    <div class="col-header Mon">Mon</div>
-    <div class="col-header Tue">Tue</div>
-    <div class="col-header Wed">Wed</div>
-    <div class="col-header Thu">Thu</div>
-    <div class="col-header Fri">Fri</div>
-    <div class="col-header Sat">Sat</div>
-    <div class="col-header Sun">Sun</div>
-    <div class="row-header row-header-1">This Week</div>
-    <div class="row-header row-header-2">Last Week</div>
-    <div class="row-header row-header-3">2 Weeks Ago</div>
-    <div class="row-header row-header-4">3 Weeks Ago</div>
-    
-    <base-pie
-      v-for="object in chartData"
-      :key="object"
-      :chartData="object"
-    ></base-pie>
-    
+    <div class="calendar-grid">
+      <div class="blank"></div>
+      <div class="col-header Mon">Mon</div>
+      <div class="col-header Tue">Tue</div>
+      <div class="col-header Wed">Wed</div>
+      <div class="col-header Thu">Thu</div>
+      <div class="col-header Fri">Fri</div>
+      <div class="col-header Sat">Sat</div>
+      <div class="col-header Sun">Sun</div>
+      <div class="row-header row-header-1">This Week</div>
+      <div class="row-header row-header-2">Last Week</div>
+      <div class="row-header row-header-3">2 Weeks Ago</div>
+      <div class="row-header row-header-4">3 Weeks Ago</div>
+
+      <base-pie
+        v-for="object in stepsMonthlyReport"
+        :key="object.dateTime"
+        :chartData="object.data"
+      ></base-pie>
     </div>
   </base-card>
 </template>
 
 <script>
+import dummyData from './dummyData.js'
 export default {
   data() {
     return {
-      chartData: [
-        {
-          datasets: [
-            {
-              data: [1, 2],
-              backgroundColor: ["#6E34EA", "#E3E3E3"],
-              borderWidth: 0
-            }
-          ]
-        }
-      ], 
+      // stepsMonthlyReport: [
+      //   {
+      //     dateTime: "a date",
+      //     data: [1, 2]
+      //   },
+      // ],
+      stepsMonthlyReport: dummyData.stepsMonthlyReport
     };
   }
 };
@@ -46,17 +42,17 @@ export default {
 
 <style scoped>
 .calendar-grid {
-  padding: 1rem .75rem;
+  padding: 1rem 0.75rem;
   display: grid;
   grid-template-rows: 12px repeat(4, 60px);
   grid-template-columns: repeat(8, 60px);
-  grid-template-areas: 
-  ". mon tue wed thu fri sat sun"
-  "thisweek none none none none none none none"
-  "lastweek none none none none none none none"
-  "wkprev2 none none none none none none none"
-  "wkprev3 none none none none none none none";
-  
+  grid-template-areas:
+    ". mon tue wed thu fri sat sun"
+    "thisweek none none none none none none none"
+    "lastweek none none none none none none none"
+    "wkprev2 none none none none none none none"
+    "wkprev3 none none none none none none none";
+
   grid-gap: 1px 6px;
 }
 .col-header {
